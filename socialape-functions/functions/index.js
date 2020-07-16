1d23eb9131a5bc5f9e94400db902301dc7a19e78
markchristian1337 <markchristian1337@yahoo.com>
2020-07-15 19:22:29 -0500

    create screams

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -1,9 +1,49 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+var serviceAccount = require("C:\Src\socialape\socialape-functions\functions\node_modules\serviceAccountKey.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://socialape-c3879.firebaseio.com"
+  });
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello world!");
+});
+
+exports.getScreams = functions.https.onRequest((req, res) => {
+    admin.firestore().collection('screams')
+        .get()
+        .then((data) =>{
+            let screams = [];
+            data.forEach((doc) => {
+                screams.push(doc.data());
+            });
+            return res.json(screams);
+        })
+        .catch((err) => console.error(err));
+});
+
+exports.createScream = functions.https.onRequest((req, res) => {
+    const newScream = {
+        body: req.body.body,
+        userHandle: req.body.userHandle,
+        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    };
+
+    admin
+        .firestore()
+        .collection('screams')
+        .add(newScream)
+        .then((doc) => {
+            res.json({ message: `document ${doc.id} created successfully` });
+        })
+        .catch((err) => {
+            res.status(500).json({ error: 'something went wrong' });
+            console.error(err);
+        });
+});
