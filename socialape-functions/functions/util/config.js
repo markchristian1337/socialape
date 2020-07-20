477d2d52fe71baef3e3375487ecb43c02f900b4a
markchristian1337 <markchristian1337@yahoo.com>
2020-07-20 18:03:35 -0500

    add image upload 2

---
+++ socialape-functions/functions/util/config.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+module.exports = {
+  apiKey: process.env.API_KEY,
+  authDomain: "socialape-c3879.firebaseapp.com",
+  databaseURL: "https://socialape-c3879.firebaseio.com",
+  projectId: "socialape-c3879",
+  storageBucket: "socialape-c3879.appspot.com",
+  messagingSenderId: "407597475413",
+  appId: "1:407597475413:web:f18e78f2ccb90cbdea16dc",
+  measurementId: "G-FHQLMWXCVQ"
+};
