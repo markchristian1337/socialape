848f4a7ffaa0c5632d469416dfb9224fc6599277
markchristian1337 <markchristian1337@yahoo.com>
2020-07-23 13:59:24 -0500

    add user profile details

--- socialape-functions/functions/dbschema.js
+++ socialape-functions/functions/dbschema.js
@@ -1,11 +1,34 @@
-let db = {}
+let db = {
   screams: [
     {
-      userHandle: 'user',
-      body: 'this is the scream body',
-      createdAt: '2020-07-17T06:05:03.132Z',
+      userHandle: "user",
+      body: "this is the scream body",
+      createdAt: "2020-07-17T06:05:03.132Z",
       likeCount: 5,
-      commentCount: 2
-    }
-  ]
-}
+      commentCount: 2,
+    },
+  ],
+};
+
+const userDetails = {
+  //Redux data
+  credentials: {
+    bio: "I love Aisha",
+    createdAt: "2020-07-17T22:00:20.205Z",
+    email: "user@email.com",
+    handle: "user",
+    location: "Chicago, IL",
+    userId: "sqe0QXpLnnbscThoa6MyQ9CTwy13",
+    website: "https://google.com",
+  },
+  likes: [
+    {
+      userHandle: "user",
+      screamId: "1234567890",
+    },
+    {
+      userHandle: "user",
+      screamId: "0987654321",
+    },
+  ],
+};
