c9fffbfdbf520e6283640c754f11e59208ce99b2
markchristian1337 <markchristian1337@yahoo.com>
2020-07-29 18:58:58 -0500

    add like, unlike, and delete post

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -1,37 +1,40 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
 
-const app = require("express")();
+const app = require('express')();
 
-const FBAuth = require("./util/fbAuth");
+const FBAuth = require('./util/fbAuth');
 
 const {
   getAllScreams,
   postOneScream,
   getScream,
   commentOnScream,
-} = require("./handlers/screams");
+  likeScream,
+  unlikeScream,
+  deleteScream,
+} = require('./handlers/screams');
 const {
   signup,
   login,
   uploadImage,
   addUserDetails,
   getAuthenticatedUser,
-} = require("./handlers/users");
+} = require('./handlers/users');
 
 // Scream routes
-app.get("/screams", getAllScreams);
-app.post("/scream", FBAuth, postOneScream);
-app.get("/scream/:screamId", getScream);
-// TODO: delete scream
-// TODO: like a scream
-// TODO: unlike a scream
-app.post("/scream/:screamId/comment", FBAuth, commentOnScream);
+app.get('/screams', getAllScreams);
+app.post('/scream', FBAuth, postOneScream);
+app.get('/scream/:screamId', getScream);
+app.delete('/scream/:screamId', FBAuth, deleteScream);
+app.get('/scream/:screamId/like', FBAuth, likeScream);
+app.get('/scream/:screamId/unlike', FBAuth, unlikeScream);
+app.post('/scream/:screamId/comment', FBAuth, commentOnScream);
 
 // Users routes
-app.post("/signup", signup);
-app.post("/login", login);
-app.post("/user/image", FBAuth, uploadImage);
-app.post("/user", FBAuth, addUserDetails);
-app.get("/user", FBAuth, getAuthenticatedUser);
+app.post('/signup', signup);
+app.post('/login', login);
+app.post('/user/image', FBAuth, uploadImage);
+app.post('/user', FBAuth, addUserDetails);
+app.get('/user', FBAuth, getAuthenticatedUser);
 
 exports.api = functions.https.onRequest(app);
