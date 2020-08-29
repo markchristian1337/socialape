4aa6dc5e711143cf63566731e71d4d7c1c15dc07
markchristian1337 <markchristian1337@yahoo.com>
2020-08-29 16:55:29 -0500

    mark notifications read

--- socialape-functions/functions/dbschema.js
+++ socialape-functions/functions/dbschema.js
@@ -1,19 +1,29 @@
 let db = {
   screams: [
     {
-      userHandle: "user",
-      body: "this is the scream body",
-      createdAt: "2020-07-17T06:05:03.132Z",
+      userHandle: 'user',
+      body: 'this is the scream body',
+      createdAt: '2020-07-17T06:05:03.132Z',
       likeCount: 5,
       commentCount: 2,
     },
   ],
   comments: [
     {
-      userHandle: "user",
-      screamId: "adsfkjdsfjkfks",
-      body: "Comment from dbschema!",
-      createdAt: "2020-01-01T10:59:52.798Z",
+      userHandle: 'user',
+      screamId: 'adsfkjdsfjkfks',
+      body: 'Comment from dbschema!',
+      createdAt: '2020-01-01T10:59:52.798Z',
+    },
+  ],
+  notifications: [
+    {
+      recipient: 'user',
+      sender: 'john',
+      read: 'true | false ',
+      screamId: 'kdjsfgdksuufhgkdsufky',
+      type: 'like | comment',
+      createdAt: '2019-03-15T10:59:52.798Z',
     },
   ],
 };
@@ -21,22 +31,22 @@ let db = {
 const userDetails = {
   //Redux data
   credentials: {
-    bio: "I love Aisha",
-    createdAt: "2020-07-17T22:00:20.205Z",
-    email: "user@email.com",
-    handle: "user",
-    location: "Chicago, IL",
-    userId: "sqe0QXpLnnbscThoa6MyQ9CTwy13",
-    website: "https://google.com",
+    bio: 'I love Aisha',
+    createdAt: '2020-07-17T22:00:20.205Z',
+    email: 'user@email.com',
+    handle: 'user',
+    location: 'Chicago, IL',
+    userId: 'sqe0QXpLnnbscThoa6MyQ9CTwy13',
+    website: 'https://google.com',
   },
   likes: [
     {
-      userHandle: "user",
-      screamId: "1234567890",
+      userHandle: 'user',
+      screamId: '1234567890',
     },
     {
-      userHandle: "user",
-      screamId: "0987654321",
+      userHandle: 'user',
+      screamId: '0987654321',
     },
   ],
 };
--- socialape-functions/functions/handlers/users.js
+++ socialape-functions/functions/handlers/users.js
@@ -1,16 +1,16 @@
-const { admin, db } = require("../util/admin");
+const { admin, db } = require('../util/admin');
 
-const config = require("../util/config");
+const config = require('../util/config');
 
-const firebase = require("firebase");
+const firebase = require('firebase');
 firebase.initializeApp(config);
 
 const {
   validateSignupData,
   validateLoginData,
   reduceUserDetails,
-} = require("../util/validators");
-const { UserRecordMetadata } = require("firebase-functions/lib/providers/auth");
+} = require('../util/validators');
+const { UserRecordMetadata } = require('firebase-functions/lib/providers/auth');
 
 //Sign users up
 exports.signup = (req, res) => {
@@ -25,14 +25,14 @@ exports.signup = (req, res) => {
 
   if (!valid) return res.status(400).json(errors);
 
-  const noImg = "no-img.png";
+  const noImg = 'no-img.png';
 
   let token, userId;
   db.doc(`/users/${newUser.handle}`)
     .get()
     .then((doc) => {
       if (doc.exists) {
-        return res.status(400).json({ handle: "this handle is already taken" });
+        return res.status(400).json({ handle: 'this handle is already taken' });
       } else {
         return firebase
           .auth()
@@ -59,8 +59,8 @@ exports.signup = (req, res) => {
     })
     .catch((err) => {
       console.error(err);
-      if (err.code === "auth/email-already-in-use") {
-        return res.status(400).json({ email: "Email is already in use" });
+      if (err.code === 'auth/email-already-in-use') {
+        return res.status(400).json({ email: 'Email is already in use' });
       } else {
         return res.status(500).json({ error: err.code });
       }
@@ -89,10 +89,10 @@ exports.login = (req, res) => {
     })
     .catch((err) => {
       console.error(err);
-      if (err.code === "auth/wrong-password") {
+      if (err.code === 'auth/wrong-password') {
         return res
           .status(403)
-          .json({ general: "Wrong credentials, please try again" });
+          .json({ general: 'Wrong credentials, please try again' });
       } else return res.status(500).json({ error: err.code });
     });
 };
@@ -104,13 +104,52 @@ exports.addUserDetails = (req, res) => {
   db.doc(`/users/${req.user.handle}`)
     .update(userDetails)
     .then(() => {
-      return res.json({ message: "Details added successfully" });
+      return res.json({ message: 'Details added successfully' });
     })
     .catch((err) => {
       console.error(err);
       return res.status(500).json({ error: err.code });
     });
 };
+
+//Get any user details
+exports.getUserDetails = (req, res) => {
+  let userData = {};
+  db.doc(`/users/${req.params.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        userData.user = doc.data();
+        return db
+          .collection('screams')
+          .where('userHandle', '==', req.params.handle)
+          .orderBy('createdAt', 'desc')
+          .get();
+      } else {
+        return res.status(404).json({ error: 'User not found ' });
+      }
+    })
+    .then((data) => {
+      userData.screams = [];
+      data.forEach((doc) => {
+        userData.screams.push({
+          body: doc.data().body,
+          createdAt: doc.data().createdAt,
+          userHandle: doc.data().userHandle,
+          userImage: doc.data().userImage,
+          likeCount: doc.data().likeCount,
+          commentCount: doc.data().commentCount,
+          screamId: doc.id,
+        });
+      });
+      return res.json(userData);
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+};
+
 //Get own user details
 exports.getAuthenticatedUser = (req, res) => {
   let userData = {};
@@ -120,8 +159,8 @@ exports.getAuthenticatedUser = (req, res) => {
       if (doc.exists) {
         userData.credentials = doc.data();
         return db
-          .collection("likes")
-          .where("userHandle", "==", req.user.handle)
+          .collection('likes')
+          .where('userHandle', '==', req.user.handle)
           .get();
       }
     })
@@ -129,6 +168,26 @@ exports.getAuthenticatedUser = (req, res) => {
       userData.likes = [];
       data.forEach((doc) => {
         userData.likes.push(doc.data());
+      });
+      return db
+        .collection('notifications')
+        .where('recipient', '==', req.user.handle)
+        .orderBy('createdAt', 'desc')
+        .limit(10)
+        .get();
+    })
+    .then((data) => {
+      userData.notifications = [];
+      data.forEach((doc) => {
+        userData.notifications.push({
+          recipient: doc.data().recipient,
+          sender: doc.data().sender,
+          createdAt: doc.data().createdAt,
+          screamId: doc.data().screamId,
+          type: doc.data().type,
+          read: doc.data().read,
+          notificationId: doc.id,
+        });
       });
       return res.json(userData);
     })
@@ -140,22 +199,22 @@ exports.getAuthenticatedUser = (req, res) => {
 
 // Upload a profile image for user
 exports.uploadImage = (req, res) => {
-  const BusBoy = require("busboy");
-  const path = require("path");
-  const os = require("os");
-  const fs = require("fs");
+  const BusBoy = require('busboy');
+  const path = require('path');
+  const os = require('os');
+  const fs = require('fs');
 
   const busboy = new BusBoy({ headers: req.headers });
 
   let imageFileName;
   let imageToBeUploaded = {};
 
-  busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
-    if (mimetype !== "image/jpeg" && mimetype != "image/png") {
-      return res.status(400).json({ error: "Wrong file type submitted" });
+  busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+    if (mimetype !== 'image/jpeg' && mimetype != 'image/png') {
+      return res.status(400).json({ error: 'Wrong file type submitted' });
     }
     // my.image.png
-    const imageExtension = filename.split(".")[filename.split(".").length - 1];
+    const imageExtension = filename.split('.')[filename.split('.').length - 1];
     // 23454325623452.png
     imageFileName = `${Math.round(
       Math.random() * 100000000000
@@ -164,7 +223,7 @@ exports.uploadImage = (req, res) => {
     imageToBeUploaded = { filepath, mimetype };
     file.pipe(fs.createWriteStream(filepath));
   });
-  busboy.on("finish", () => {
+  busboy.on('finish', () => {
     admin
       .storage()
       .bucket()
@@ -181,7 +240,7 @@ exports.uploadImage = (req, res) => {
         return db.doc(`/users/${req.user.handle}`).update({ imageUrl });
       })
       .then(() => {
-        return res.json({ message: "Image uploaded successfully" });
+        return res.json({ message: 'Image uploaded successfully' });
       })
       .catch((err) => {
         console.error(err);
@@ -189,4 +248,21 @@ exports.uploadImage = (req, res) => {
       });
   });
   busboy.end(req.rawBody);
+};
+
+exports.markNotificationsRead = (req, res) => {
+  let batch = db.batch();
+  req.body.forEach((notificationId) => {
+    const notification = db.doc(`/notifications/${notificationId}`);
+    batch.update(notification, { read: true });
+  });
+  batch
+    .commit()
+    .then(() => {
+      return res.json({ message: 'Notifications marked read' });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
 };
